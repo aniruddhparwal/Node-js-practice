2b4ebc7aff9d49d5c0a7b78935597035f8ac5e43
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-30 21:01:04 +0530

    multiple url

--- practice 1/first.js
+++ practice 1/first.js
@@ -3,6 +3,16 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     console.log("new request recevied");
     var url = req.url;
+    if (url == "/books")
+        res.end("list of books")
+    if (url == "/checkout")
+        res.end("it is checkout")
+    if (url == "/favicon.ico") {
+        console.log("404 favicon")
+        res.writeHead(404)
+        res.end()
+    }
+
     console.log("ani" + url)
     res.end("Online")
 });
